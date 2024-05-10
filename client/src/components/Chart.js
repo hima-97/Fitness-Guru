ce41cc0c0390c8a271ed52417a5c48d10afa7259
hima-97 <hima.kumar97@gmail.com>
2024-05-09 17:55:27 -0700

    Changes to files in client/src/components directory

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,77 +1,131 @@
-// This file is used for the "Chart" component
-// The Chart.js file is a React component that uses the Chart.js library to create a line chart
-// The component receives props from its parent component, and uses them to render a line chart with the given data, options and chart title
+// The Chart.js file in the Fitness Guru application defines a component that displays a customizable line chart using the react-chartjs-2 library.
 
-import React, {Component} from 'react';
-import {Bar, Line} from 'react-chartjs-2';
+// **Imports**
+// It imports:
+// - **React:** Main library for building user interfaces.
+// - **react-chartjs-2 Library:** Provides chart components for React.
+// - **Chart.js Components (Line):** Line chart component used for rendering the chart.
 
-// function called Chart which takes in a props object as a parameter:
-// Inside the function, it sets some default props, such as 'displayLegend' and 'legendPosition' which are used to configure the chart's legend
-function Chart(props){
+// **Chart Component**
+// A functional component that receives several props:
+// - `chartData`: Data to be displayed in the chart.
+// - `exercise`: Name of the exercise being charted.
+// - `repetitions`: Number of repetitions for the exercise.
+// - **Return Statement:**
+//   - Wraps the chart in a div with the class `chartContainer`.
+//   - Displays a `Line` chart with the provided `chartData`.
+//   - Configures the chart with customizable options:
+//     - **Title:** Displays a chart title based on `exercise` and `repetitions`.
+//     - **Legend:** Configurable display of chart legend.
+//     - **Axis Properties:** Customizable axis properties based on `chartData`.
+
+// **Summary**
+// - **Chart Display Workflow:**
+//   - Renders a line chart with the given data, options, and chart title.
+//   - Provides customizable legend and axis properties.
+// - **Props and Configuration Options:**
+//   - Accepts props like `chartData`, `exercise`, and `repetitions` to configure the chart.
+//   - Configures title, legend, and axis options based on the provided props.
+
+// Example Usage:
+
+// <Chart
+//   chartData={chartData}
+//   exercise="Bench Press"
+//   repetitions={10}
+// />
+
+
+
+
+// Importing necessary components from React and react-chartjs-2 library
+import React, { Component } from "react";
+import { Bar, Line } from "react-chartjs-2";
+
+// The Chart component definition
+// Receives a props object as a parameter, which includes chart data and various options
+function Chart(props) {
+  // Setting up some default properties for the chart legend
   const defaultProps = {
-    displayLegend: true,
-    legendPosition:'left',
-  }
-  // the component returns JSX code that will be rendered to the page:
-  // The JSX code includes a div with a class of "chart" and inside it, there is a Line component from the Chart.js library 
-  // The Line component takes in several properties such as data, width, and height, which are passed in as props from the parent component 
-  // The Line component also has options property, which is an object that contains several properties such as the pointRadius, pointBackgroundColor, pointBorderColor, borderColor and plugins
-  // The plugins property is also an object, which contains properties such as title, legend and scales
-  // The title property is an object that sets the title of the chart, the legend property is also an object that sets the legend of the chart, and the scales property is an object that sets the scales of the chart
-    return (
-      <div className="chart">
-        <Line
-          data={props.chartData}
-          width={40}
-	        height={20}
-          options={{
-            pointRadius: 6,
-            pointBackgroundColor: 'Red',
-            pointBorderColor: 'Gray',
-            borderColor: 'Gray',
-            plugins:{
-              title: {
-                display: true,
-                text:'Weight Changes for '+ props.exercise + ", " + props.repetitions + ' Repetitions',
-                color: 'rgba(54, 162, 235, 0.6)', 
-                font: { size: 30, weight:'900'}
-              },
-              legend:{
-                display:props.displayLegend,
-                position:props.legendPosition
-              }
+    displayLegend: true, // Legend is displayed by default
+    legendPosition: "left", // Legend position is on the left by default
+  };
+
+  // The component returns JSX code that will be rendered to the page
+  // It includes a div with a class of "chart", inside which is a Line chart
+  // The Line component takes several properties like `data`, `width`, `height`, and `options`
+  // These properties are passed in as props from the parent component
+  return (
+    <div className="chart">
+      {/* Rendering the Line chart with the provided data and options */}
+      <Line
+        data={props.chartData} // Chart data passed in from the parent component
+        width={40} // Chart width
+        height={20} // Chart height
+        options={{
+          // Chart appearance options
+          pointRadius: 6, // Radius of data points
+          pointBackgroundColor: "Red", // Background color of data points
+          pointBorderColor: "Gray", // Border color of data points
+          borderColor: "Gray", // Border color of the chart
+
+          // Plugins configuration: title, legend, etc.
+          plugins: {
+            // Configuring the chart title
+            title: {
+              display: true, // Display the title
+              text:
+                "Weight Changes for " +
+                props.exercise +
+                ", " +
+                props.repetitions +
+                " Repetitions", // Dynamic chart title based on exercise and repetitions
+              color: "rgba(54, 162, 235, 0.6)", // Title color
+              font: { size: 30, weight: "900" }, // Title font size and weight
             },
-            scales: {
-              y: {
-                title:{
-                  display: true,
-                  text: 'Weight (lb)',
-                  color: 'rgba(54, 162, 235, 0.6)', 
-                  font: { size: 25, weight:'900'}
-                },
-                ticks: {
-                  callback: function(value, index, values) {
-                    return value + ' lb';
-                  },
-                  font: {size: 15}
-                }
+            // Configuring the legend
+            legend: {
+              display: props.displayLegend || defaultProps.displayLegend, // Display legend based on prop or default value
+              position: props.legendPosition || defaultProps.legendPosition, // Legend position based on prop or default value
+            },
+          },
+
+          // Scales configuration: x and y axes
+          scales: {
+            // Y-axis configuration (Weight axis)
+            y: {
+              title: {
+                display: true, // Display the axis title
+                text: "Weight (lb)", // Y-axis title
+                color: "rgba(54, 162, 235, 0.6)", // Axis title color
+                font: { size: 25, weight: "900" }, // Axis title font size and weight
               },
-              x: {
-                title:{
-                  display: true,
-                  text: 'Time',
-                  color: 'rgba(54, 162, 235, 0.6)',               
-                  font: { size: 25, weight:'900'}
+              ticks: {
+                // Customizing tick labels
+                callback: function (value, index, values) {
+                  return value + " lb"; // Append "lb" to each tick label
                 },
-                ticks: {
-                  font: {size: 16}
-                }
-              }
-            }
+                font: { size: 15 }, // Tick label font size
+              },
+            },
+            // X-axis configuration (Time axis)
+            x: {
+              title: {
+                display: true, // Display the axis title
+                text: "Time", // X-axis title
+                color: "rgba(54, 162, 235, 0.6)", // Axis title color
+                font: { size: 25, weight: "900" }, // Axis title font size and weight
+              },
+              ticks: {
+                // Customizing tick labels
+                font: { size: 16 }, // Tick label font size
+              },
+            },
+          },
         }}
-    />
-      </div>
-    )
-  }
+      />
+    </div>
+  );
+}
 
 export default Chart;
